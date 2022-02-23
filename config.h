ec7c570242ec2d8a3184eb713a76b3dafa6649f4
cjhead <chead@linuxmail.org>
2022-02-23 12:12:11 -0500

    Fixes default foreground color

--- config.h
+++ config.h
@@ -149,9 +149,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-unsigned int defaultcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 256;
 unsigned int bg = 0;
 unsigned int bgUnfocused = 0;
